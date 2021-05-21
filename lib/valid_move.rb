3a94e3a0cce5713c51699d35eac5f29d7ec4b2ec
Motti Wilschanski <mchillywilly@gmail.com>
2021-05-21 01:22:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,14 @@
-def valid_move?
-
-
-  def position_taken?(board, index)
-
-
-
-     taken = nil
-     if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-       taken = false
-     elseif
-       taken = true
-     else 2.between?(0, 8)
+def valid_move?(board, index)
+  if index.between?(1, 9)
+    if !position_taken?(board, index)
+      true
     end
+  end
+
+
+  index.between?(0,8) && !position_taken?(board, index)
+  end
+  
+  def position_taken?(board, index)
+    board[index] != " "
   end
